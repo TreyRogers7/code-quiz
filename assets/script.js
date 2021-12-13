cabd046bc2ffd2c21a1b9b5225c5f7d73f6eb9ba
Richard Rogers <richardrogers@Richards-MacBook-Pro.local>
2021-12-12 20:58:29 -0700

    code quiz5

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,62 @@
-var questionsEl = document.querySelector(".questions")
-var answersEl = document.querySelector(".answers")
-var answerBtnEl = document.querySelectorAll(".answerBtn")
+var questionsEl=document.querySelector(".questions")
+var answersEl=document.querySelector(".answers")
+var answerBtnEl=document.querySelectorAll(".answerBtn")
+var startbtnEl=document.querySelector(".startBtn")
+var timerEl=document.querySelector(".timer")
+var h1El=document.querySelector(".h1")
+var containerEl=document.querySelector("#container")
 console.log(answerBtnEl)
+
+startbtnEl.addEventListener('click', beginGame)
+
+function beginGame() {
+    console.log('started')
+    startbtnEl.classList.add('hide')
+    h1El.classList.add('hide')
+    // answersEl.classList.remove('hide')
+    // questionsEl.classList.remove('hide')
+    containerEl.classList.remove('hide')
+}
+
+function countDown() {
+    var timeLeft=60;
+
+    var timerInterval = setInterval(function () {
+        if (timeLeft > 1) {
+        timerEl.textContent = timeLeft + " seconds left.";
+        timeLeft--;
+        }
+        else if (timeLeft === 1) {
+          timerEl.textContent = timeLeft + " second left.";
+          timeLeft--;
+        } else {
+          timerEl.textContent = '';
+          clearInterval(timerInterval);
+          displayMessage();
+        }
+      }, 1000);
+      }
+
 var questionsArray = [{
     question: "what you eat?",
     answers: ["meat", "veggies", "fruits", "dessert"],
     solution: "meat"
+},{
+    question: "where do you work",
+    answers: ["golf course", "sotware engineer", "server", "water boy"],
+    solution: "golf course"
+},{
+    question: "where do you work",
+    answers: ["golf course", "sotware engineer", "server", "water boy"],
+    solution: "golf course"
+},{
+    question: "where do you work",
+    answers: ["golf course", "sotware engineer", "server", "water boy"],
+    solution: "golf course"
+},{
+    question: "where do you work",
+    answers: ["golf course", "sotware engineer", "server", "water boy"],
+    solution: "golf course"
 },{
     question: "where do you work",
     answers: ["golf course", "sotware engineer", "server", "water boy"],
