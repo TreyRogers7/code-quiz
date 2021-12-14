fad4b216d00b09675274299921871c8f47885905
Richard Rogers <richardrogers@Richards-MacBook-Pro.local>
2021-12-13 18:29:43 -0700

    code quiz Mental Meltdown

--- assets/script.js
+++ assets/script.js
@@ -1,123 +1,20 @@
-var questionsEl=document.querySelector(".questions")
-var answersEl=document.querySelector(".answers")
-var answerBtnEl=document.querySelectorAll(".answerBtn")
-var startbtnEl=document.querySelector(".startBtn")
-var timerEl=document.querySelector(".time")
-var h1El=document.querySelector(".h1")
-var containerEl=document.querySelector("#container")
-var highscoresEl=document.querySelector(".highscores")
-console.log(answerBtnEl)
+var startBtnEl=document.querySelector('.startBtn')
+var questionEl=document.querySelector('#question-container')
 
-// var shuffleQuestions, currentQuestionIndex
+startBtnEl.addEventListener('click', beginGame)
 
-startbtnEl.addEventListener('click', beginGame)
+function beginGame(){
+    startBtnEl.classList.add('.hide')
+    questionEl.classList.remove('.hide')
 
-function beginGame() {
-    console.log('started')
-    startbtnEl.classList.add('hide')
-    h1El.classList.add('hide')
-    answersEl.classList.remove('hide')
-    questionsEl.classList.remove('hide')
-    // displayQuestion()
-    // provideNextQuestion()
     console.log(beginGame)
 }
 
-var secondsLeft=60;
-
-function setTime() {
-
-    var timerInterval = setInterval(function() {
-        secondsLeft--;
-        timerEl.textContent = secondsLeft + " game over in ";
-    
-        if(secondsLeft === 0) {
-          // Stops execution of action at set interval
-          clearInterval(timerInterval);
-          // Calls function to create and append image
-          endGame();
-        }
-      }, 1000);
-    }
-
-// function displayMessage() {
-//     var timecount=0;
-
-//     var messageinterval=setInterval(function() {
-//         if (timer[timecount] === undefined) {
-//             clearInterval(msgInterval);
-//           } else {
-//             timerEl.textContent = time[timecount];
-//             wordCount++;
-//           }
-//         }, 1000);
-//     }
-
-
-// }    
-
-var questionsArray = [{
-    question: "what you eat?",
-    answers: [
-        {text:"meat", correct:true},
-        {text:"veggies", correct:false},
-        {text:"fruits", correct:false},
-        {text:"dessert", correct:false}],
-},{
-    question: "where do you work",
-    answers: ["golf course", "sotware engineer", "server", "water boy"],
-    solution: "golf course"
-},{
-    question: "where do you work",
-    answers: ["golf course", "sotware engineer", "server", "water boy"],
-    solution: "golf course"
-},{
-    question: "where do you work",
-    answers: ["golf course", "sotware engineer", "server", "water boy"],
-    solution: "golf course"
-},{
-    question: "where do you work",
-    answers: ["golf course", "sotware engineer", "server", "water boy"],
-    solution: "golf course"
-},{
-    question: "where do you work",
-    answers: ["golf course", "sotware engineer", "server", "water boy"],
-    solution: "golf course"
-}];
-console.log(questionsArray)
-
-var qCounter = 0
-
-<<<<<<< HEAD
-// function provideNextQuestion() {
-//     displayQuestion(shuffleQuestions[currentQuestionIndex])
-// }
-=======
-function provideNextQuestion() {
-    displayQuestion(shuffleQuestions[currentQuestionIndex])
+function setNextQuestion(){
+    console.log(setNextQuestion)
 }
 
-function displayQuestion(questionsArray) {
-    questionsEl.innerText=questionsArray.questionsArray
-// } jashdfkj
->>>>>>> temp
-
-// function displayQuestion(questionsArray) {
-    // questionsEl.innerText=questionsArray.questionsArray
-// }
-
-{
-    var questionObj=questionsArray[qCounter];
-    questionsEl.textContent=questionObj.question;
-    for (var i=0; i<answerBtnEl.length; i++) {
-        answerBtnEl[i].textContent=questionObj.answers[i]
-    }
+function selectAnswer(){
+    console.log(selectAnswer)
 }
-// console.log(provideNextQuestion)
 
-// provideNextQuestion();
-
-function endGame() {
-    highscoresEl.classList.remove('hide')
-    console.log(endGame)
-}
