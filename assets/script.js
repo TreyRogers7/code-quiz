e32e19aa3eec7678478e5e5edd07473b3cfea305
Richard Rogers <richardrogers@Richards-MacBook-Pro.local>
2021-12-13 21:13:25 -0700

    check user response, button color change, timer

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,14 @@
-var questionsEl=document.querySelector(".questions")
-var answersEl=document.querySelector(".answers")
-var answerBtnEl=document.querySelectorAll(".answerBtn")
-var startbtnEl=document.querySelector(".startBtn")
-var timerEl=document.querySelector(".timer")
-var h1El=document.querySelector(".h1")
-var containerEl=document.querySelector("#container")
+var questionsEl = document.querySelector(".questions")
+var answersEl = document.querySelector(".answers")
+var answerBtnEl = document.querySelectorAll(".answerBtn")
+var startbtnEl = document.querySelector(".startBtn")
+var timerEl = document.querySelector(".timer")
+var h1El = document.querySelector(".h1")
+var containerEl = document.querySelector("#container")
+var timerInterval
 console.log(answerBtnEl)
 
-startbtnEl.addEventListener('click', beginGame)
+// startbtnEl.addEventListener('click', beginGame)
 
 function beginGame() {
     console.log('started')
@@ -16,48 +17,54 @@ function beginGame() {
     // answersEl.classList.remove('hide')
     // questionsEl.classList.remove('hide')
     containerEl.classList.remove('hide')
+    displayQuestion()
+    countDown()
+}
+
+function endGame() {
+    containerEl.classList.add('hide')
+    clearInterval(timerInterval)
 }
 
 function countDown() {
-    var timeLeft=60;
+    var timeLeft = 60;
 
-    var timerInterval = setInterval(function () {
+    timerInterval = setInterval(function () {
         if (timeLeft > 1) {
-        timerEl.textContent = timeLeft + " seconds left.";
-        timeLeft--;
+            timerEl.textContent = timeLeft + " seconds left.";
+            timeLeft--;
         }
         else if (timeLeft === 1) {
-          timerEl.textContent = timeLeft + " second left.";
-          timeLeft--;
+            timerEl.textContent = timeLeft + " second left.";
+            timeLeft--;
         } else {
-          timerEl.textContent = '';
-          clearInterval(timerInterval);
-          displayMessage();
+            timerEl.textContent = '';
+            endGame();
         }
-      }, 1000);
-      }
+    }, 1000);
+}
 
 var questionsArray = [{
     question: "what you eat?",
     answers: ["meat", "veggies", "fruits", "dessert"],
     solution: "meat"
-},{
+}, {
     question: "where do you work",
     answers: ["golf course", "sotware engineer", "server", "water boy"],
     solution: "golf course"
-},{
-    question: "where do you work",
+}, {
+    question: "where do you sit",
     answers: ["golf course", "sotware engineer", "server", "water boy"],
     solution: "golf course"
-},{
-    question: "where do you work",
+}, {
+    question: "where do you lay",
     answers: ["golf course", "sotware engineer", "server", "water boy"],
     solution: "golf course"
-},{
-    question: "where do you work",
+}, {
+    question: "where do you stand",
     answers: ["golf course", "sotware engineer", "server", "water boy"],
     solution: "golf course"
-},{
+}, {
     question: "where do you work",
     answers: ["golf course", "sotware engineer", "server", "water boy"],
     solution: "golf course"
@@ -65,11 +72,43 @@ var questionsArray = [{
 var qCounter = 0
 
 function displayQuestion() {
-    var questionObj=questionsArray[qCounter];
-    questionsEl.textContent=questionObj.question;
-    for (var i=0; i<answerBtnEl.length; i++) {
-        answerBtnEl[i].textContent=questionObj.answers[i]
+    var questionObj = questionsArray[qCounter];
+    questionsEl.textContent = questionObj.question;
+    for (var i = 0; i < answerBtnEl.length; i++) {
+        answerBtnEl[i].textContent = questionObj.answers[i]
     }
 }
 
-displayQuestion();
+function solution(event) {
+    var questionObj = questionsArray[qCounter];
+    if (event.target.matches('button')) {
+        var userAnswer = event.target.textContent
+        if (userAnswer === questionObj.solution) {
+            event.target.classList.add("correct");
+            setTimeout(function (){
+                event.target.classList.remove("correct")
+            },1000)
+    
+        } else {
+            event.target.classList.add("wrong");
+            setTimeout(function (){
+                event.target.classList.remove("wrong")
+            },1000)
+    
+        }
+        if (qCounter<questionsArray.length-1){
+            console.log(qCounter)
+            console.log(questionsArray.length)
+        setTimeout(function (){
+            qCounter++
+            displayQuestion();
+        },1000)}
+        else {
+            // endgame function goes here
+           endGame()
+        }
+    }
+}
+
+answersEl.addEventListener('click', solution)
+startbtnEl.addEventListener('click', beginGame)
