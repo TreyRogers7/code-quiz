420312ad729f1089c2b7f18f8922d7f883693f82
Richard Rogers <richardrogers@Richards-MacBook-Pro.local>
2021-12-13 20:22:24 -0700

    revert

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,14 @@
-var startBtnEl=document.querySelector('.startBtn')
-var questionEl=document.querySelector('#question-container')
+var questionsEl=document.querySelector(".questions")
+var answersEl=document.querySelector(".answers")
+var answerBtnEl=document.querySelectorAll(".answerBtn")
+var startbtnEl=document.querySelector(".startBtn")
+var timerEl=document.querySelector(".timer")
+var h1El=document.querySelector(".h1")
+var containerEl=document.querySelector("#container")
+console.log(answerBtnEl)
 
-startBtnEl.addEventListener('click', beginGame)
+startbtnEl.addEventListener('click', beginGame)
 
-<<<<<<< HEAD
 function beginGame() {
     console.log('started')
     startbtnEl.classList.add('hide')
@@ -14,21 +19,57 @@ function beginGame() {
 }
 
 function countDown() {
-    var timeLeft=59;
-=======
-function beginGame(){
-    startBtnEl.classList.add('.hide')
-    questionEl.classList.remove('.hide')
->>>>>>> fad4b216d00b09675274299921871c8f47885905
+    var timeLeft=60;
 
-    console.log(beginGame)
+    var timerInterval = setInterval(function () {
+        if (timeLeft > 1) {
+        timerEl.textContent = timeLeft + " seconds left.";
+        timeLeft--;
+        }
+        else if (timeLeft === 1) {
+          timerEl.textContent = timeLeft + " second left.";
+          timeLeft--;
+        } else {
+          timerEl.textContent = '';
+          clearInterval(timerInterval);
+          displayMessage();
+        }
+      }, 1000);
+      }
+
+var questionsArray = [{
+    question: "what you eat?",
+    answers: ["meat", "veggies", "fruits", "dessert"],
+    solution: "meat"
+},{
+    question: "where do you work",
+    answers: ["golf course", "sotware engineer", "server", "water boy"],
+    solution: "golf course"
+},{
+    question: "where do you work",
+    answers: ["golf course", "sotware engineer", "server", "water boy"],
+    solution: "golf course"
+},{
+    question: "where do you work",
+    answers: ["golf course", "sotware engineer", "server", "water boy"],
+    solution: "golf course"
+},{
+    question: "where do you work",
+    answers: ["golf course", "sotware engineer", "server", "water boy"],
+    solution: "golf course"
+},{
+    question: "where do you work",
+    answers: ["golf course", "sotware engineer", "server", "water boy"],
+    solution: "golf course"
+}];
+var qCounter = 0
+
+function displayQuestion() {
+    var questionObj=questionsArray[qCounter];
+    questionsEl.textContent=questionObj.question;
+    for (var i=0; i<answerBtnEl.length; i++) {
+        answerBtnEl[i].textContent=questionObj.answers[i]
+    }
 }
 
-function setNextQuestion(){
-    console.log(setNextQuestion)
-}
-
-function selectAnswer(){
-    console.log(selectAnswer)
-}
-
+displayQuestion();
